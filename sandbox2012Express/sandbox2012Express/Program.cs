2eb5d652af860128a10194e66192a352cdc44901
A <Man>
2014-04-05 15:08:07 +0300

    First commit
    Committing a hello world console app

---
+++ sandbox2012Express/sandbox2012Express/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+namespace sandbox2012Express
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(  "Hello World");
+        }
+    }
+}
